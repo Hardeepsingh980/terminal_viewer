0335af6b6ed902ac978ac56aa7fc8aa9f8ae920b
Hardeep Singh <hardeep0khalsa122@gmail.com>
2020-06-21 06:43:59 +0530

    v0.3

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ from terminal_viewer import show_image
 
 show_image('hello.jpg', 100)
 ```
+
+#### Note Resize the terminal window for better results.
+#### It Only works in bash terminal.
+
 ## Screenshots
 
 ![alt text](https://github.com/Hardeepsingh980/terminal_viewer/blob/master/screenshots/1.jpg?raw=true)
--- setup.py
+++ setup.py
@@ -3,22 +3,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name = 'teminal_viewer',         # How you named your package folder (MyLib)
-  packages = ['teminal_viewer'],   # Chose the same as "name"
-  version = '0.1,      # Start with a small number and increase it with every change you make
+  name = 'terminal_viewer',         # How you named your package folder (MyLib)
+  packages = ['terminal_viewer'],   # Chose the same as "name"
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Package for printing images in terminal',   # Give a short description about your library
   long_description =long_description,
   long_description_content_type = 'text/markdown',
   author = 'Hardeep SIngh',                   # Type in your name
   author_email = 'hardeep0khalsa122@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Hardeepsingh980/DeeType.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Hardeepsingh980/DeeType/archive/master.zip',    # I explain this later on
+  url = 'https://github.com/Hardeepsingh980/terminal_viewer',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Hardeepsingh980/terminal_viewer.git',    # I explain this later on
   keywords = ['terminal','image','terminal_image','terminal_viewer'],   # Keywords that define your package best
   install_requires=[
       'numpy',
       'Pillow',
-      'sys'
   ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
